6a09066b27d0bf693c2126bd6d086442462340b6
macbellingrath <macbellingrath@gmail.com>
2015-06-25 11:41:29 -0500

    Merge

---
+++ Stormy/NetworkOperation.swift
@@ -0,0 +1,47 @@
+//
+//  NetworkOperation.swift
+//  Stormy
+//
+//  Created by Mac Bellingrath on 6/25/15.
+//  Copyright (c) 2015 com.mbellingrath. All rights reserved.
+//
+
+
+import Foundation
+
+class NetworkOperation {
+    
+    lazy var config: NSURLSessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
+    lazy var session: NSURLSession = NSURLSession(configuration: self.config)
+    let queryURL: NSURL
+    
+    typealias JSONDictionaryCompletion = ([String: AnyObject]? -> Void)
+    
+    init(url: NSURL) {
+        self.queryURL = url
+    }
+    
+    func downloadJSONFromURL(completion: JSONDictionaryCompletion) {
+        
+        let request = NSURLRequest(URL: queryURL)
+        let dataTask = session.dataTaskWithRequest(request) {
+            (let data, let response, let error) in
+            
+            // 1. Check HTTP response for successful GET request
+            if let httpResponse = response as? NSHTTPURLResponse {
+                switch httpResponse.statusCode {
+                case 200:
+                    // 2. Create JSON object with data
+                    let jsonDictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? [String: AnyObject]
+                    completion(jsonDictionary)
+                default:
+                    println("GET request not successful. HTTP status code: \(httpResponse.statusCode)")
+                }
+            } else {
+                println("Error: Not a valid HTTP response")
+            }
+        }
+        
+        dataTask.resume()
+    }
+}
